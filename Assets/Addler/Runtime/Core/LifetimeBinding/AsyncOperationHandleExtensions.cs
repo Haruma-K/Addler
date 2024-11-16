31850a452ac04bbfe6f9b67b75f4aa0efdd0d566
Haruki Yano <haruki.y.11g@gmail.com>
2024-11-16 23:20:12 +0900

    fix a bug when releasing scene

--- Assets/Addler/Runtime/Core/LifetimeBinding/AsyncOperationHandleExtensions.cs
+++ Assets/Addler/Runtime/Core/LifetimeBinding/AsyncOperationHandleExtensions.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
 
 namespace Addler.Runtime.Core.LifetimeBinding
 {
@@ -14,11 +15,11 @@ namespace Addler.Runtime.Core.LifetimeBinding
         /// <param name="gameObject"></param>
         /// <returns></returns>
         /// <exception cref="ArgumentNullException"></exception>
-        public static AsyncOperationHandle BindTo(this AsyncOperationHandle self, GameObject gameObject)
+        public static AsyncOperationHandle BindTo(this AsyncOperationHandle self, GameObject gameObject, bool isScene)
         {
             if (gameObject == null)
             {
-                Addressables.Release(self);
+                ReleaseHandle(self, isScene);
                 throw new ArgumentNullException(nameof(gameObject),
                     $"{nameof(gameObject)} is null so the handle can't be bound and will be released immediately.");
             }
@@ -26,7 +27,7 @@ namespace Addler.Runtime.Core.LifetimeBinding
             if (!gameObject.TryGetComponent(out MonoBehaviourBasedReleaseEvent releaseEvent))
                 releaseEvent = gameObject.AddComponent<MonoBehaviourBasedReleaseEvent>();
 
-            return BindTo(self, releaseEvent);
+            return BindTo(self, releaseEvent, isScene);
         }
 
         /// <summary>
@@ -41,12 +42,12 @@ namespace Addler.Runtime.Core.LifetimeBinding
         {
             if (gameObject == null)
             {
-                Addressables.Release(self);
+                ReleaseHandle(self, typeof(T) == typeof(SceneInstance));
                 throw new ArgumentNullException(nameof(gameObject),
                     $"{nameof(gameObject)} is null so the handle can't be bound and will be released immediately.");
             }
 
-            ((AsyncOperationHandle)self).BindTo(gameObject);
+            ((AsyncOperationHandle)self).BindTo(gameObject, typeof(T) == typeof(SceneInstance));
             return self;
         }
 
@@ -57,18 +58,18 @@ namespace Addler.Runtime.Core.LifetimeBinding
         /// <param name="releaseEvent"></param>
         /// <returns></returns>
         /// <exception cref="ArgumentNullException"></exception>
-        public static AsyncOperationHandle BindTo(this AsyncOperationHandle self, IReleaseEvent releaseEvent)
+        public static AsyncOperationHandle BindTo(this AsyncOperationHandle self, IReleaseEvent releaseEvent, bool isScene)
         {
             if (releaseEvent == null)
             {
-                Addressables.Release(self);
+                ReleaseHandle(self, isScene);
                 throw new ArgumentNullException(nameof(releaseEvent),
                     $"{nameof(releaseEvent)} is null so the handle can't be bound and will be released immediately.");
             }
 
             void OnRelease()
             {
-                Addressables.Release(self);
+                ReleaseHandle(self, isScene);
                 releaseEvent.Dispatched -= OnRelease;
             }
 
@@ -88,13 +89,21 @@ namespace Addler.Runtime.Core.LifetimeBinding
         {
             if (releaseEvent == null)
             {
-                Addressables.Release(self);
+                ReleaseHandle(self, typeof(T) == typeof(SceneInstance));
                 throw new ArgumentNullException(nameof(releaseEvent),
                     $"{nameof(releaseEvent)} is null so the handle can't be bound and will be released immediately.");
             }
 
-            ((AsyncOperationHandle)self).BindTo(releaseEvent);
+            ((AsyncOperationHandle)self).BindTo(releaseEvent, typeof(T) == typeof(SceneInstance));
             return self;
+        }
+
+        private static void ReleaseHandle(AsyncOperationHandle handle, bool isScene)
+        {
+            if (isScene)
+                Addressables.UnloadSceneAsync(handle);
+            else
+                Addressables.Release(handle);
         }
     }
 }
